beb9be4b846d93b2d799fdbe87163c886adb0bec
ci_cd_eaioss <ci_cd_eaioss@gitlab.com>
2022-04-27 12:01:26 +0200

    add config files

--- MicroProfile/events-app/src/main/java/com/proof/of/concept/model/Event.java
+++ MicroProfile/events-app/src/main/java/com/proof/of/concept/model/Event.java
@@ -3,20 +3,13 @@ package com.proof.of.concept.model;
 import jakarta.json.bind.annotation.JsonbDateFormat;
 import jakarta.json.bind.annotation.JsonbTransient;
 import jakarta.validation.constraints.NotEmpty;
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.Document;
-import org.bson.codecs.jsr310.LocalDateTimeCodec;
 
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Data
 @NoArgsConstructor
@@ -38,12 +31,12 @@ public class Event implements Serializable {
 //    private Collection<Picture> images = new HashSet<>();
 
     @JsonbTransient
-    public Document getEventDoc(){
+    public Document getEventDoc() {
         Document doc = new Document();
-        doc.put("title",title);
-        doc.put("artist",artist.getArtistDoc());
-        doc.put("location",location.getLocationDoc());
-        doc.put("date",date.toString());
+        doc.put("title", title);
+        doc.put("artist", artist.getArtistDoc());
+        doc.put("location", location.getLocationDoc());
+        doc.put("date", date.toString());
 //        doc.put("images",images);
         return doc;
     }
